e7619d45439c46a1a315c4b50a412f2998872d5c
jimboqu <jamesm.quilter@gmail.com>
2022-07-17 15:25:15 +0100

    removed old code

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+import pandas
+
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
